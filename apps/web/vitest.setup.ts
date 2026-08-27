8c96691e2d57cbf82ec373ec81cb5802c6f89479
anubhav-kayal <kayalanubhav14@gmail.com>
2026-08-27 15:31:48 +0530

    chore(web): configure stats dependencies and testing

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -19,3 +19,38 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: () => false,
   }),
 });
+
+class ResizeObserverStub implements ResizeObserver {
+  readonly #callback: ResizeObserverCallback;
+
+  constructor(callback: ResizeObserverCallback) {
+    this.#callback = callback;
+  }
+
+  observe(target: Element): void {
+    this.#callback(
+      [
+        {
+          target,
+          contentRect: DOMRectReadOnly.fromRect({ width: 640, height: 224 }),
+          borderBoxSize: [],
+          contentBoxSize: [],
+          devicePixelContentBoxSize: [],
+        },
+      ],
+      this,
+    );
+  }
+
+  unobserve(): void {}
+  disconnect(): void {}
+}
+
+Object.defineProperty(window, "ResizeObserver", {
+  writable: true,
+  value: ResizeObserverStub,
+});
+
+HTMLElement.prototype.getBoundingClientRect = function getBoundingClientRect() {
+  return new DOMRect(0, 0, 640, 224);
+};
